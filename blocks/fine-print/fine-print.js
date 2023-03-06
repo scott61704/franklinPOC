6f6f258aafc64393a4c33a396652f6e3848e1006
Scott Weaver <scott@weaverclan.org>
2023-03-06 05:50:12 -0600

    added the hr tag for the fine-print block and cleaned up unused code from its js file, and removed the pink background from the body tag

--- blocks/fine-print/fine-print.js
+++ blocks/fine-print/fine-print.js
@@ -1,16 +1,4 @@
-/* async function fetchFragments(fragmentAnchors) {
-    var promises;
-    promises.add(fragmentAnchors.forEach((anchor) => fetch(anchor.href+".plain.html"​)));
-    
-    var returnResults;
-    Promise.allSettled(promises).
-        then((results) => results.forEach((result) => returnResults.add(result)));
-
-    return returnResults;
-  }
-*/
 async function getAllUrls(urls) {
-    //alert("url0 = "+ urls[0]);
     try {
         var data = await Promise.all(
             urls.map(
@@ -30,36 +18,21 @@ async function getAllUrls(urls) {
 }
 
 export default async function decorate(block) {
-    //block.textContent = '';
-    // Can't use a normal like the following, because you can't have an async within it
-    // [...block.querySelectorAll("a[href*='/fragment/']")].forEach(el => {
     
+    const hrElement = document.createElement("hr");
+    block.prepend(hrElement);
+
     const anchors = [...block.querySelectorAll("a[href*='/fragment/']")];
 
     var responses = await getAllUrls(anchors)
-    //alert("responses.length = " + responses.length);
-
-    //fetchFragments(block.querySelectorAll("a[href*='/fragment/']"));
-    /*if (anchors.length > 0) {
-           fetchMoviesAndCategories().then(([movies, categories]) => {
-            movies;     // fetched movies
-            categories; // fetched categories
-          }).catch(error => {
-            // /movies or /categories request failed
-          });
 
         // Fetching the .plain.html version of the URL gets just the contents of the body, it seems.
-        var url = anchors[0].href+".plain.html"; */
         var index = 0;
         var response = responses[index];
         var fragmentDiv;
         while (response) {
-            //alert("url = " + url);
-            //const resp = await fetch(url);
-            //alert(response.ok);
             if (response) {
-                const fragmentText = response; //await response.text();
-                //alert ("fragment content = " + fragmentText);
+                const fragmentText = response; 
                 fragmentDiv = document.createElement("div");
                 fragmentDiv.className = "fine-print-item";
                 fragmentDiv.innerHTML = fragmentText;
@@ -69,29 +42,12 @@ export default async function decorate(block) {
                 anchors[index].className = "fine-print-item";                
             }
             index++;
-            //alert(index);
             if (index < responses.length) {
                 response = responses[index];
             } else {
                 response = null;
-            //}
         }
 
     }
 
-/*    [...block.querySelectorAll("a[href*='/fragment/']")].forEach(el => {
-        // Do something with each element
-        //alert("got an a  " + el.getAttribute("href"));
-       
-        const href= el.getAttribute("href");
-        alert ("fragment href = " + el.href);
-        
-        const resp = await fetch(el.href);
-        if (resp.ok) {
-            const html = await resp.text();
-            alert ("fragment content = " + html);
-        }
-    });
-    
-  */ 
 }
