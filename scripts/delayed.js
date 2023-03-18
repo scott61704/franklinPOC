13b66136787cc2967af73e91235c479ced296a9d
Scott Weaver <scott@weaverclan.org>
2023-03-18 06:24:55 -0500

    adjust styles to minimize CLS.  So, defined some style changes to cause mobile sizes for headings, etc. to be the default ones.  Still getting some CLS for h1 from 1x, doesn't happen if the heading1x.css file isn't loaded

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,12 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './lib-franklin.js';
+//import { loadHeader } from './lib-franklin.js';
+//import { loadFooter } from './lib-franklin.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
+console.log("DELAYED - at beginning")
+  //loadHeader(document.querySelector('header'));
+  //loadFooter(document.querySelector('footer'));
 
 // add more delayed functionality here
